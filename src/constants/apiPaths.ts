183cbdd03e3c25639bc9e2a23e79d0524b28102e
Dmitry Serediuk <k0sm0s@tut.by>
2021-09-21 22:59:30 +0300

    update: update import API PATH

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://6ljyzakuy6.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ilbc1s6l6f.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://m4pe19u35i.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
